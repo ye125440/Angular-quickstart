dfd64ac19cb25ad41ceb7a9b3eb4cd3c18260d85
叶佳栩 <yejiaxu@sf-express.com>
2017-06-09 10:55:54 +0800

    quickstart

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+// bootstrap the AppModule in a main.ts file
+platformBrowserDynamic().bootstrapModule(AppModule);
